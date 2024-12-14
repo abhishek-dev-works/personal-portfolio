09a8f9a992c3e4502ba6fa5b6d0fb8df0718fe94
Abhishek <abhishek.works22@gmail.com>
2024-12-14 22:33:45 +0530

    Updated resume link

--- src/Components/ResumeDownload.tsx
+++ src/Components/ResumeDownload.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const resLinkk =
-  "https://drive.google.com/uc?export=download&id=15WXRyV9pg3U8imlP4vmofJg2rz89AE0O";
+  "https://drive.google.com/uc?export=download&id=1nbLUbHiAPKo14IteIXKl4na4COMSiRyp";
 const ResumeDownload = () => {
   const handleClick = () => {
     window.location.href = resLinkk;
